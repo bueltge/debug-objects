75aab8b8188b8c8c84b703091450017fde7d321f
bueltge <frank@bueltge.de>
2013-09-08 22:36:41 +0200

    fix cookie function to cache active tab

--- js/debug_objects.dev.js
+++ js/debug_objects.dev.js
@@ -1,4 +1,4 @@
-function toggle(obj) {
+function toggle( obj ) {
 	var el = document.getElementById(obj);
 	if ( el.style.display != 'block' ) {
 		el.style.display = 'block';
@@ -10,17 +10,50 @@ function toggle(obj) {
 ( function( $ ) {
 	
 	$(function() {
-		$( '#debugobjectstabs' ).tabs( {
-			cache: true,
-			cookie: { expires: 7 }
-		} );
-	} );
+		
+		$( "#debugobjectstabs" ).tabs( {
+			select: function(event, ui) {
+				window.location.replace(ui.tab.hash);
+			},
+			create: function(e, ui) {
+				var tabs = $(e.target);
+				// Get the value from the cookie
+				var value = $.cookie('selected-tab');
 	
+				if(value) {
+					console.log('Setting value %s', value);
+					// Set which tab should be selected. Older jQuery UI API
+					tabs.tabs('select',value);
+				}
+			},
+		} );
+		
+		$(".ui-tabs-anchor").click(function() {
+			var value = $(this).attr( "id" );
+			// cut value and -1. tab counter start with 0
+			value = jQuery.trim( value ).substring(6, 10) - 1;
+			$.cookie( 'debug-objects-selected-tab', value, { expires: 7 } );
+		});
+		
+	});
+	
+	// read cookie
+	// $.cookie( 'debug-objects-selected-tab', { path: '/' } );
+	var selected_tab = $.cookie( 'debug-objects-selected-tab' );
+	if ( typeof selected_tab  == 'undefined' ) selected_tab = 0;
+	
+	$( '#debugobjectstabs' ).tabs( {
+		active: selected_tab,
+	} );
+	/*
+	 * only useful before WP version 3.4
+	 *
 	$( '#wpadminbar' ).click( function() {
 		$( 'html, body' ).animate( { scrollTop: 0 }, 100 );
 	} );
 	$( '#wpadminbar li' ).click( function( e ) { 
 		e.stopPropagation(); 
 	} );
+	*/
 	
 } )( jQuery );
