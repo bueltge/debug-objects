f544cfbd11522c832df6f78cb64a35e166d5e096
Frank Bueltge <frank@bueltge.de>
2013-11-20 14:30:16 +0100

    change version for new dev

--- debug_objects.php
+++ debug_objects.php
@@ -9,11 +9,11 @@
  * Text Domain: debug_objects
  * Domain Path: /languages
  * Description: List filter and action-hooks, cache data, defined constants, qieries, included scripts and styles, php and memory informations and return of conditional tags only for admins; for debug, informations or learning purposes. Setting output in the settings of the plugin and use output via link in Admin Bar, via setting, via url-param '<code>debug</code>' or set a cookie via url param '<code>debugcookie</code>' in days.
- * Version:     2.1.16
+ * Version:     2.1.17-RC1
  * License:     GPLv3
  * Author:      Frank Bültge
  * Author URI:  http://bueltge.de/
- * Last Change: 11/14/2013
+ * Last Change: 11/20/2013
  */
 
 // avoid direct calls to this file, because now WP core and framework has been used.
