76a17e946a6082a271ccc3401ed148e44eba72fb
Frank Bueltge <frank@bueltge.de>
2016-09-02 15:48:28 +0200

    Switch hook list in filterable table

--- inc/class-debug_hooks.php
+++ inc/class-debug_hooks.php
@@ -34,7 +34,10 @@ if ( class_exists( 'Debug_Objects_Debug_Hooks' ) )
 			
 			return self::$classobj;
 		}
-		
+
+		/**
+		 * Debug_Objects_Debug_Hooks constructor.
+		 */
 		public function __construct() {
 			
 			if ( ! current_user_can( '_debug_objects' ) )
@@ -53,7 +56,7 @@ if ( class_exists( 'Debug_Objects_Debug_Hooks' ) )
 			);
 			*/
 			$tabs[] = array( 
-				'tab' => __( 'Debug Hooks', parent :: get_plugin_data() ),
+				'tab' => __( 'Debug Hooks', parent::get_plugin_data() ),
 				'function' => array( $this, 'list_hooks' )
 			);
 			
@@ -67,13 +70,18 @@ if ( class_exists( 'Debug_Objects_Debug_Hooks' ) )
 			ksort( $hooks );
 			
 			echo '<h2>Hooks Total: ' . count( $wp_filter ) . '</h2>';
-			
+
+			echo  '<table class="tablesorter">';
+			echo '<thead><tr><th>Hook</th><th>Priority &middot; Functions</th></tr></thead>';
+
 			foreach( $hooks as $tag => $hook ) {
 				if ( FALSE === $filter || FALSE !== strpos( $tag, $filter ) ) {
-					self::dump_hook($tag, $hook);
+					$this->print_hooks( $tag, $hook );
 				}
 			}
-			
+
+			echo '</table>';
+
 			echo '<p class="alternate">Register filter/actions total: ' . self::$wp_func . '</p>';
 		}
 		
@@ -84,83 +92,98 @@ if ( class_exists( 'Debug_Objects_Debug_Hooks' ) )
 		
 			add_action( $hook, array( $this, 'list_hook_details' ), -1 );
 		}
-		
-		public static function list_hook_details( $input = NULL ) {
+
+		/**
+		 * @param null $input
+		 *
+		 * @return null
+		 */
+		public function list_hook_details( $input = NULL ) {
 			global $wp_filter;
 
 			$tag = current_filter();
 			if ( isset( $wp_filter[$tag] ) )
-				self::dump_hook( $tag, $wp_filter[$tag] );
+				$this->print_hooks( $tag, $wp_filter[ $tag ] );
 
 			return $input;
 		}
-		
-		public function dump_hook( $tag, $hook, $echo = TRUE ) {
-			
-			ksort($hook);
-			
-			// Get settings
-			$options = Debug_Objects_Settings::return_options();
-			
-			// Filter Debug Objects Hooks
-			if ( 
-				isset( $options[ 'filter' ] ) 
-				&& '1' === $options[ 'filter' ] 
-				&& Debug_Objects::array_find( 'Debug_Objects', $hook )
-				) {
-				return NULL;
-			}
-			
-			// Filter hook, files from this plugin, not helpful
-			if (
-				isset( $options[ 'filter' ] ) 
-				&& '1' === $options[ 'filter' ] 
-				&& preg_match( '/debug_objects/', $tag )
-				)
-				return NULL;
-			
-			$tag = esc_html( $tag );
-			
-			$output = '<ul><li><strong>' . $tag . '</strong><ul>';
 
+		/**
+		 * Print Hooks in table.
+		 *
+		 * @param $tag
+		 * @param $hook
+		 */
+		public function print_hooks( $tag, $hook ) {
+
+			if ( $this->filter_plugin_hooks( $tag, $hook ) )
+				return;
+
+			// hook, priority, function
+			$output = '<tr><td><code>' . $tag . '</code></td><td>';
 			foreach( $hook as $priority => $functions ) {
-				
-				$output .= '<li>Priority: ' . $priority . '<ul>';
-				
+
+				$output .= $priority . ' &middot; ';
+
 				foreach( $functions as $function ) {
-					
-					if ( $function['function'] != 'list_hook_details' ) {
-						
-						$output .= "\t" . '<li>&gt;&gt;&gt;&gt;&gt; <code>';
-						
+
+					if ( $function['function'] !== 'list_hook_details' ) {
+
+						$output .= "\t" . '<code>';
+
 						if ( is_string( $function['function'] ) )
 							$output .= $function['function'];
 						// @see https://github.com/jeremeamia/super_closure/blob/master/SuperClosure.class.php
 						else if ( $function['function'] instanceOf Closure )
 							$output .= '(object) <a href="http://php.net/manual/en/functions.anonymous.php" title="read more about Closures on php.net">Closure, Anonymous functions</a>';
-						
-						else if ( is_object( $function['function'][0] ) ) 
-							$output .= '(object) ' . get_class( $function['function'][0] ) . ' :: ' . $function['function'][1];
-						
+
+						else if ( is_object( $function['function'][0] ) )
+							$output .= '(object) ' . get_class( $function['function'][0] ) . '::' . $function['function'][1];
+
 						else if ( is_string( $function['function'][0] ) )
-							$output .= $function['function'][0] . ' :: ' . $function['function'][1];
-						
+							$output .= $function['function'][0] . '::' . $function['function'][1];
+
 						else
 							$output .= print_r( $function, TRUE );
-						
-						$output .= '</code> (Accepted args: ' . $function['accepted_args'] . ') </li>';
+
+						$output .= '</code> (Accepted args: ' . $function['accepted_args'] . ') <br>';
 					}
 					self::$wp_func ++;
 				}
-				$output .=  '</ul></li>';
 			}
-		
-			$output .=  '</ul></li></ul>';
-			
-			if ( $echo )
-				echo $output;
+			$output .= '</td></tr>';
 
-			return $output;
+			echo $output;
 		}
-		
+
+		/**
+		 * Check if the filter to filter functions from the plugin active.
+		 *
+		 * @return bool
+		 */
+		private function filter_plugin_hooks( $tag, $hook ) {
+
+			// Get settings
+			$options = Debug_Objects_Settings::return_options();
+
+			// Filter Debug Objects Hooks
+			if (
+				isset( $options[ 'filter' ] )
+				&& '1' === $options[ 'filter' ]
+				&& Debug_Objects::array_find( 'Debug_Objects', $hook )
+			) {
+				return TRUE;
+			}
+
+			// Filter hook, files from this plugin, not helpful
+			if (
+				isset( $options[ 'filter' ] )
+				&& '1' === $options[ 'filter' ]
+				&& preg_match( '/debug_objects/', $tag )
+			)
+				return TRUE;
+
+			return FALSE;
+		}
+
 	} // end class
